5805b722377715715bd8242105bd34d4cd875d0c
ImaanBontle <80769699+ImaanBontle@users.noreply.github.com>
2022-11-15 01:02:25 +0000

    Finalised changelog

--- update_log.sh
+++ update_log.sh
@@ -8,9 +8,9 @@ fi
 echo v${version}: >> tmp.txt
 echo ----- >> tmp.txt
 cat changes.txt >> tmp.txt
-echo >> tmp.txt
+echo "" >> tmp.txt
 cat changelog.txt >> tmp.txt
 mv changelog.txt oldchanges.txt
 mv tmp.txt changelog.txt
 rm backup_new.txt backup_changes.txt
-git commit -a
+git commit -a -m "Finalised changelog"
