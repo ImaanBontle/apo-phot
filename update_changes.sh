3a1e2602d4f26b456cd964739fd50feea7b27342
ImaanBontle <80769699+ImaanBontle@users.noreply.github.com>
2022-11-15 18:44:40 +0000

    Accidentally left out the files lol

--- update_changes.sh
+++ update_changes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cat new_changes.txt >> tmp.txt
 cat recent_changes.txt >> tmp.txt
+cat new_changes.txt >> tmp.txt
 mv new_changes.txt new_changes_backup.txt
 mv recent_changes.txt changes_backup.txt
 mv tmp.txt recent_changes.txt
